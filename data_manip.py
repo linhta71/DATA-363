63ef77332c9bfe13797c0a26e7f24bf5b66e8309
Hung Tran <Pegasucksgg@Gmail.com>
2020-07-03 18:43:06 -0700

    renamed project_data_.csv to project_data_states.csv
    delete the old project_data_states.csv

--- data_manip.py
+++ data_manip.py
@@ -9,7 +9,7 @@ import csv
 
 def main():
     old_mtx = []
-    with open("data/project_data_states.csv", mode = 'r') as old_file:
+    with open("data/project_data.csv", mode = 'r') as old_file:
         reader = csv.reader(old_file)
         old_mtx = [row for row in reader]
     # old_mtx: list of list
@@ -17,7 +17,7 @@ def main():
     # Split location into location and state (split by delim of "."
     reform.split_column(old_mtx, 2, lambda x: ("City", "State"),
                         lambda s: (s[:-4], s[-2:]))
-    retrieve_data.serialize_list(old_mtx, data_fname = "project_data_.csv")
+    retrieve_data.serialize_list(old_mtx, data_fname = "project_data_states.csv")
     return (old_mtx)
     
 if __name__ == "__main__":
